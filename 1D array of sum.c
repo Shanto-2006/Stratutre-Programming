dab55733d137bc505959081af8fcb9255ab15a44
Md. Shanto <md.shanto2006a@gmail.com>
2026-09-04 23:37:30 +0600

    1d array

---
+++ 1D array of sum.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,a[5];
+    for(i=0;i<5;i++){
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<5;i++){
+        printf("the numbers = %d\n",a[i]);
+    }
+    int  sum;
+    int avg;
+    for(i=0;i<5;i++){
+        sum=sum+a[i];
+    }
+    printf("the number of sum = %d",sum);
+    return 0;
+}
